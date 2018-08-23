a1c4df7a08cd342e03104329c7bf31bf86309fb8
Adrien Baudet <baudetad@gmail.com>
2018-08-23 16:15:52 +0200

    Finsied simplest version
    Finished simplest version in ruby

--- Ruby/simple_book/main.rb
+++ Ruby/simple_book/main.rb
@@ -1,8 +1,41 @@
 require "json"
 
-puts Dir["/data/*"].to_s
-# vocab = JSON.parse(File.open("vocab.json", "r"){|file| file.read})
-# pages = JSON.parse(File.open("data/pages.json", "r"){|file| file.read})
+def print_next(liste)
+  liste.each_with_index do |item, index|
+    puts "#{index} : #{item["action"]}"
+  end
+end
 
-# puts vocab.to_s
-# puts pages.to_s
+def get_nexte(length, vocab)
+  while true
+    puts vocab["Ask_input"]
+    entree = gets.strip
+    if (0...length).include?(entree.to_i) && entree.to_i.to_s == entree
+      break
+    end
+    puts vocab["Input_error"].sub("{}", 0.to_s).sub("{}", (length - 1).to_s)
+  end
+  entree.to_i
+end
+
+vocab = JSON.parse(File.open("data/vocab.json", "r"){|file| file.read})
+pages_file = JSON.parse(File.open("data/pages.json", "r"){|file| file.read})
+
+pages = Hash.new
+pages_file.each do |i|
+  pages[i["name"]] = i
+end
+
+puts pages.to_s
+
+actual_page = pages["main"]
+while actual_page != nil
+  # puts actual_page.to_s
+  puts actual_page["text"]
+  nexte = actual_page["next"]
+  print_next(nexte)
+  choice = get_nexte(nexte.length, vocab)
+  puts nexte[choice]["page"]
+  actual_page = pages[nexte[choice]["page"]]
+end
+vocab["End"]
