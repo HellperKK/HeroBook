9a7ca0d8e30dcd7ad3f124371173467a6079d711
Adrien Baudet <baudetad@gmail.com>
2019-02-14 20:57:14 +0100

    Finishing haxe version of simple_book

--- Haxe/simple_book/main.hx
+++ Haxe/simple_book/main.hx
@@ -1,5 +1,6 @@
 import sys.io.File;
 import Sys;
+using StringTools;
 
 typedef VocabWord = {
   var name:String;
@@ -22,16 +23,71 @@ class Main {
     // vocab
     var tmp:String = File.getContent("data/vocab.json");
     var vocabArr:Array<VocabWord> = haxe.Json.parse(tmp);
-    var vocab = [for (i in vocabArr) i.name => i.text];
+    var vocab:Map<String, String> = [for (i in vocabArr) i.name => i.text];
 
     // pages
     var tmp:String = File.getContent("data/pages.json");
     var pagesArr:Array<Page> = haxe.Json.parse(tmp);
     var book = [for (i in pagesArr) i.name => i];
-    trace(book);
+    run(book, vocab);
   }
 
   static function run(book, vocab) {
+    var actual = "main";
+    var page = book.get(actual);
+    while(page != null) {
+      var next:Array<PageNext> = page.next;
+      show_text(page.text);
+      show_next(next);
+      var num = getInput(next.length, vocab);
+      actual = next[num].page;
+      page = book.get(actual);
+    }
+    Sys.println(vocab.get("End"));
+  }
 
+  static function show_text(text:String) {
+
+    if (text.length <= 80){
+      Sys.println(text);
+    }
+    else {
+      var i = 79;
+      while ((text.charAt(i) != ' ') && i >= 0){
+        i -= 1;
+      }
+      if (i < 0) {
+        i = 79;
+      }
+      else {
+        i += 1;
+      }
+      Sys.println(text.substring(0, i).trim());
+      show_text(text.substring(i));
+    }
+  }
+
+  static function show_next(next:Array<PageNext>) {
+    for( i in 0...next.length ) {
+      Sys.println('${i} ${next[i].action}');
+    }
+  }
+
+  static function getInput(max, vocab:Map<String, String>) {
+    var stdin = Sys.stdin();
+    while(true) {
+      Sys.println(vocab.get("Ask_input"));
+      var entry = Std.parseInt(stdin.readLine());
+      var inputError = vocab.get("Input_error").replace("{min}", "0").replace("{max}", Std.string(max - 1));
+      if (entry == null){
+        Sys.println(inputError);
+        continue;
+      }
+      if ((entry < 0) || (entry >= max)) {
+        Sys.println(inputError);
+        continue;
+      }
+      return entry;
+    }
   }
 }
