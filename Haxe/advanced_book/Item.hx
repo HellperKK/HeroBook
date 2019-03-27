ec4b0fde15befed37e0d3de22f5099f4a694a289
Adrien Baudet <baudetad@gmail.com>
2019-03-27 20:13:05 +0100

    started item

--- Haxe/advanced_book/Item.hx
+++ Haxe/advanced_book/Item.hx
@@ -1,9 +1,19 @@
 class Item{
-  public var type:String;
-  private var actions:Map<String, Void->Void>
+  private var type:String;
+  private var actions:Map<String, Array<String>->Void>
 
   public function new(type) {
     this.type = type;
     actions = new Map<String, Void->Void>();
+  }
+
+  public function addAction(name, action){
+    actions.set(name, action);
+  }
+
+  public function activate(name, elms){
+    if(actions.exist(name)){
+      actions[name](elms)
+    }
   }
 }
