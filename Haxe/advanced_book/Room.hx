6e4adcddf84bef61247fb0534432cd702fe0e0e0
Adrien Baudet <baudetad@gmail.com>
2019-03-28 17:27:24 +0100

    update

--- Haxe/advanced_book/Room.hx
+++ Haxe/advanced_book/Room.hx
@@ -1,6 +1,13 @@
 class Room extends Item{
 
+  private var content:Array<Item>;
+
   public function new(type, desc) {
     super(type, desc);
+    content = [];
+  }
+
+  public function add(item) {
+    content.push(item);
   }
 }
