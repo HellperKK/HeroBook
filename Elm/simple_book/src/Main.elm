479c8fe4ac150cea542a9dc8e5a23619d8eb81c5
Adrien Baudet <baudetad@gmail.com>
2019-04-05 15:24:56 +0200

    Second monster beaten

--- Elm/simple_book/src/Main.elm
+++ Elm/simple_book/src/Main.elm
@@ -14,20 +14,25 @@ main =
 
 type alias ChoiceModel =
   { action : String
-  , next : String
+  , page : String
+  }
+
+type alias PageModel =
+  { name : String
+  , text : String
+  , next : List ChoiceModel
   }
 
 type alias Model =
-  { dico : Dict.Dict String ChoiceModel
+  { pages : List PageModel
   , actual : String
   }
 
 init : Model
 init =
-  {dico = Dict.fromList
-    [ ("First", {action = "parler un peu", next = "Second"})
-    , ("Second"  , {action = "parler beaucoup", next = "Third"})
-    , ("Third", {action = "parler mal", next = "First"})
+  {pages =
+    [{name = "First", text = "First page", next = [{action="Suivant", page="Second"}]}
+    , {name = "Second", text = "Second page", next = [{action="Precedent", page="First"}]}
     ]
   , actual = "First"
   }
@@ -46,15 +51,30 @@ update msg model =
 
 
 -- VIEW
-to_buttons item = div [] [button [onClick (Choice item.next)] [text item.action]]
+to_buttons item = div [] [button [onClick (Choice item.page)] [text item.action]]
 
-recover_dico val dico = case (Dict.get val dico) of
+{--recover_dico val dico = case (Dict.get val dico) of
   Just x -> x
-  Nothing -> {action = "", next = ""}
+  Nothing -> {action = "", page = ""}
+--}
+
+find_list f liste = case liste of
+  [] -> Nothing
+  x :: xs ->
+    if f x
+    then Just x
+    else find_list f xs
+
+maybe_page page = case page of
+  Nothing -> {name = "First", text = "First page", next = []}
+  Just x -> x
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ div [] [ text ((recover_dico model.actual model.dico )).action ]
-    , div [] (List.map to_buttons (Dict.values model.dico))
-    ]
+  let
+    actual_page = (find_list (\x -> x.name == model.actual) model.pages) |> maybe_page
+  in
+    div []
+      [ div [] [ text actual_page.text]
+      , div [] (List.map to_buttons actual_page.next)
+      ]
