93118b86a356ed40bdb59f016f02423fc29fdbf6
Adrien Baudet <baudetad@gmail.com>
2023-03-16 16:04:40 +0100

    remove unused css

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+
 import App from "./App";
-import "./styles.css";
+
 import store from "./utils/state";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
