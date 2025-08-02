295185223e0c6647bd941cc2493c53c51d6694ed
Adrien Baudet <baudetad@gmail.com>
2025-08-02 14:34:03 +0200

    linting

--- src/components/layout/menuLayout/MenuButton.tsx
+++ src/components/layout/menuLayout/MenuButton.tsx
@@ -1,10 +1,14 @@
-import { MouseEventHandler, PropsWithChildren } from "react";
-import "./menuButton.scss"
+import type { MouseEventHandler, PropsWithChildren } from "react";
+import "./menuButton.scss";
 
 type Props = PropsWithChildren<{
-    onClick?: MouseEventHandler<HTMLButtonElement>
-}>
+	onClick?: MouseEventHandler<HTMLButtonElement>;
+}>;
 
-export default function MenuButton({children, onClick}: Props) {
-    return <button onClick={onClick} className="menu-button">{children}</button>
+export default function MenuButton({ children, onClick }: Props) {
+	return (
+		<button type="button" onClick={onClick} className="menu-button">
+			{children}
+		</button>
+	);
 }
