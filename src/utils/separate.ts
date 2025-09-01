4d196ba52e9cf3b08cf74e353bf1e55d05245e51
Adrien Baudet <baudetad@gmail.com>
2025-09-01 18:08:29 +0200

    add ignore

--- src/utils/separate.ts
+++ src/utils/separate.ts
@@ -4,6 +4,7 @@ export function separate<A>(values: Array<A>, separator: () => A): Array<A> {
   }
 
   const result: Array<A> = [];
+  // biome-ignore lint/style/noNonNullAssertion: allways works since arry is not empty as checked above
   result.push(values.shift()!);
 
   for (const value of values) {
