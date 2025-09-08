d6df3d156f3950be85286492e6918f321b5e61e4
Adrien Baudet <baudetad@gmail.com>
2025-09-08 14:25:59 +0200

    update renderBlock and selection color

--- src/pages/editor/blocks/RenderBlock.tsx
+++ src/pages/editor/blocks/RenderBlock.tsx
@@ -2,8 +2,8 @@ import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import type { RootState } from '../../../store/store';
 import type { Block } from '../../../utils/game/Block';
-import ButtonChoice from './ButtonChoice';
 import './renderBlock.scss';
+import { oppositeColorRGB } from '../../../utils/oppositeColorRGB';
 
 type Props = {
   block: Block;
@@ -17,18 +17,20 @@ export default function RenderBlock({ block, onClick, active }: Props) {
     pages,
     settings: { format },
   } = useSelector((state: RootState) => state.project);
-  const activeClass = active ? ' active' : '';
 
   // biome-ignore lint/style/noNonNullAssertion: will allways work
   const page = pages.find((page) => page.id === +params.id!)!;
 
+  const pageColor = page.format?.page ?? format.page;
+
   if (block.type === 'text') {
     return (
       <div
-        className={`text-block${activeClass}`}
+        className={`text-block`}
         style={{
           fontFamily: block.format?.textFont ?? page.format?.textFont ?? format.textFont,
           color: block.format?.textColor ?? page.format?.textColor ?? format.textColor,
+          border: active ? `3px solid ${oppositeColorRGB(pageColor)}` : 'none',
         }}
       >
         <div onClick={onClick}>{block.content}</div>
@@ -37,7 +39,26 @@ export default function RenderBlock({ block, onClick, active }: Props) {
   }
 
   if (block.type === 'choice') {
-    return <ButtonChoice className={activeClass} choice={block} onClick={onClick} />;
+    return (
+      <div
+        className={`button-choice`}
+        style={{
+          border: active ? `3px solid ${oppositeColorRGB(pageColor)}` : 'none',
+        }}
+      >
+        <button
+          type="button"
+          style={{
+            backgroundColor: block.format?.btnColor ?? page.format?.btnColor ?? format.btnColor,
+            color: block.format?.btnTextColor ?? page.format?.btnTextColor ?? format.btnTextColor,
+            fontFamily: block.format?.btnFont ?? page.format?.btnFont ?? format.btnFont,
+          }}
+          onClick={onClick}
+        >
+          {block.text}
+        </button>
+      </div>
+    );
   }
 
   return <div className="render-block">not managed</div>;
