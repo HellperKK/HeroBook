44e475838937a52d4b0bbdf31f307cc2cca25c9c
Adrien Baudet <baudetad@gmail.com>
2026-03-13 11:28:38 +0100

    updated tabs

--- src/utils/reactNodeToArray.ts
+++ src/utils/reactNodeToArray.ts
@@ -1,8 +1,9 @@
-import React, { ReactElement } from "react";
+import type { JSXElementConstructor, ReactElement } from 'react';
 
-export function reactElementToArray(
-  node: ReactElement | ReactElement[] | undefined
-): ReactElement[] {
+// biome-ignore lint/suspicious/noExplicitAny: uses react definition
+export function reactElementToArray<P, T extends string | JSXElementConstructor<any>>(
+  node: ReactElement<P, T> | ReactElement<P, T>[] | undefined,
+): ReactElement<P, T>[] {
   if (node === undefined) {
     return [];
   }
