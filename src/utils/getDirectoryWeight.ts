d0c6d0093d1d2d6d6eed1e4ff7db73a6ee2a0f70
Adrien Baudet <baudetad@gmail.com>
2026-03-14 13:17:02 +0100

    add utility function to get directory wheight

---
+++ src/utils/getDirectoryWeight.ts
@@ -0,0 +1,10 @@
+import { BaseDirectory, lstat } from "@tauri-apps/plugin-fs";
+import { scanDirectoryFiles } from "./scanDirectoryFiles";
+
+export async function getDirectoryWeight(path: string) {
+    const files = await scanDirectoryFiles(path, { baseDir: BaseDirectory.Document });
+    console.log('Files:', files);
+    const fileStats = await Promise.all(files.map((file) => lstat(`${file}`, { baseDir: BaseDirectory.Document })));
+    const size = fileStats.reduce((acc, fileStat) => acc + fileStat.size, 0);
+    return size;
+  }
