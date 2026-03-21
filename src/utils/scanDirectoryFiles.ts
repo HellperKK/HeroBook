a546565c6b5ff5783562ef8be5429cf2fcf09b69
Adrien Baudet <baudetad@gmail.com>
2026-03-21 17:47:45 +0100

    format code

--- src/utils/scanDirectoryFiles.ts
+++ src/utils/scanDirectoryFiles.ts
@@ -1,13 +1,13 @@
-import { BaseDirectory, readDir, ReadDirOptions } from "@tauri-apps/plugin-fs";
+import { BaseDirectory, type ReadDirOptions, readDir } from '@tauri-apps/plugin-fs';
 
 export async function scanDirectoryFiles(path: string, options: ReadDirOptions) {
-    const entries = await readDir(path, { baseDir: BaseDirectory.Document });
-    let results: Array<string> = entries.filter((entry) => entry.isFile).map(entry => `${path}/${entry.name}`);
-    for (const entry of entries) {
-      if (entry.isDirectory) {
-        const subEntries = (await scanDirectoryFiles(`${path}/${entry.name}`, options));
-        results = [...results, ...subEntries];
-      }
+  const entries = await readDir(path, { baseDir: BaseDirectory.Document });
+  let results: Array<string> = entries.filter((entry) => entry.isFile).map((entry) => `${path}/${entry.name}`);
+  for (const entry of entries) {
+    if (entry.isDirectory) {
+      const subEntries = await scanDirectoryFiles(`${path}/${entry.name}`, options);
+      results = [...results, ...subEntries];
     }
-    return results;
-  };
+  }
+  return results;
+}
