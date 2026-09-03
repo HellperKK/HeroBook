c95c362dddac381147d0079b600f56b636f6a775
Adrien Baudet <baudetad@gmail.com>
2026-09-03 16:01:49 +0200

    update imports

--- src/pages/start-page/StartPage.tsx
+++ src/pages/start-page/StartPage.tsx
@@ -1,4 +1,4 @@
-import { BaseDirectory, DirEntry, exists, readDir, readTextFile } from '@tauri-apps/plugin-fs';
+import { BaseDirectory, type DirEntry, exists, readDir, readTextFile } from '@tauri-apps/plugin-fs';
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
@@ -8,7 +8,7 @@ import ButtonGroup from '../../components/inputs/buttonGroup/buttonGroup';
 import Paper from '../../components/surfaces/paper/Paper';
 import Label from '../../components/texts/label/Label';
 import { editProject, loadProject } from '../../store/projectSlice';
-import { Project } from '../../utils/game/Project';
+import type { Project } from '../../utils/game/Project';
 import { projectsPath } from '../../utils/paths';
 
 import './startPage.scss';
